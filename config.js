87e63e1ffe686e2a80661d070367e70a2a897861
Bhushan Bapat <bhushan_bapat29@yahoo.co.in>
2022-05-08 21:02:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,8 @@ function getFile(filePath) {
   }
   return null;
 }
-
+//Function to get the File Environment
+//Args : envVariable
 function getFileEnv(envVariable) {
   const origVar = process.env[envVariable];
   const fileVar = process.env[envVariable + '_FILE'];
@@ -56,7 +57,7 @@ function getFileEnv(envVariable) {
   }
   return origVar;
 }
-
+//Functionto get BinaryFiles
 function getBinaryFileEnv(envVariable) {
   const fileVar = process.env[envVariable];
   return getFile(fileVar);
@@ -66,6 +67,7 @@ const meConfigMongodbServer = process.env.ME_CONFIG_MONGODB_SERVER
   ? process.env.ME_CONFIG_MONGODB_SERVER.split(',')
   : false;
 
+//Function to get Connection Strings
 function getConnectionStringFromEnvVariables() {
   const infos = {
     // server: mongodb hostname or IP address
